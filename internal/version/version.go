77dd803a875b82a34da0af78970031eb12f4a704
Sergio Tejón <sergio.tejon@gmail.com>
2024-10-30 13:01:40 +0100

    refactor(internal/version/version.go): Initialize new version file for better version management
    - Creation of a new go file for handling version information
    - This commit does not include any functionality yet, it's just an initial setup for better code organization

---
+++ internal/version/version.go
@@ -0,0 +1,9 @@
+package version
+
+// Version is the version of the application
+var version = "dev"
+
+// GetVersion returns the version of the application
+func GetVersion() string {
+	return version
+}
