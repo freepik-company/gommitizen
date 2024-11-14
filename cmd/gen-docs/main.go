875423fcf8b1588416266fdf788d927951edce1d
Sergio Tejón <sergio.tejon@gmail.com>
2024-11-14 15:33:12 +0100

    docs(cmd/gen-docs/main.go): Update markdown generation process to use new template and log errors using slog package

--- cmd/gen-docs/main.go
+++ cmd/gen-docs/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 
-	"github.com/freepik-company/gommitizen/internal/app/gen-docs/docs"
 	"github.com/freepik-company/gommitizen/internal/pkg/cmd"
+	"github.com/freepik-company/gommitizen/pkg/docs"
+)
+
+const (
+	templateFile = "README.md.template"
 )
 
 func main() {
 	root := cmd.Root()
 
-	err := docs.GenMarkdown(root, os.Stdout)
+	err := docs.GenMarkdown(root, os.Stdout, templateFile)
 	if err != nil {
+		slog.Error("Error generating markdown", "error", err)
 		os.Exit(1)
 	}
 }
